38fe696f8ff89f96a1f5becbf34f241c437eb4bf
unknown <royajamal2003@gmail.com>
2024-09-04 22:11:07 -0700

    index.js file end missionSlice.js file added

--- src/components/Missions/index.jsx
+++ src/components/Missions/index.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissionsData, reserveMission, leaveMission } from '../../redux/missions/MissionSlice';
 
 function Missions() {
-  return <h1>this is my mission man</h1>;
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  const reservedMissions = useSelector((state) => state.missions.reservedMissions);
+
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(fetchMissionsData());
+    }
+  }, [dispatch, missions.length]);
+
+  const handleJoinMission = (missionId) => {
+    dispatch(reserveMission(missionId));
+  };
+
+  const handleLeaveMission = (missionId) => {
+    dispatch(leaveMission(missionId));
+  };
+
+  return (
+    <div className="flex flex-col border-2 m-10 ">
+      <div className="missions-table-row head">
+        <div className="missions-table-cell mission-name">Mission</div>
+        <div className="missions-table-cell description">Description</div>
+        <div className="missions-table-cell status">Status</div>
+        <div className="missions-button-cell" />
+      </div>
+      {missions.map((mission) => (
+        <div key={mission.id} className="missions-table-row">
+          <div className="missions-table-cell mission-name">
+            {mission.mission_name}
+          </div>
+          <div className="missions-table-cell description">
+            {mission.description}
+          </div>
+          <div className="missions-table-cell status">
+            {reservedMissions.includes(mission.id) ? (
+              <span className="acmembership">Active member</span>
+            ) : (
+              <span className="membership">Not a member</span>
+            )}
+          </div>
+          <div className="missions-table-cell button">
+            {reservedMissions.includes(mission.id) ? (
+              <button onClick={() => handleLeaveMission(mission.id)} className="leave-button" type="submit">
+                Leave Mission
+              </button>
+            ) : (
+              <button onClick={() => handleJoinMission(mission.id)} className="join-button" type="submit">
+                Join Mission
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Missions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import missionsSlice from './missions/missionsSlice';
-import RocketsSlice from './Rockets/RocketsSlice';
-
-const rootReducer = combineReducers({
-  missions: missionsSlice.reducer,
-  rockets: RocketsSlice,
-});
+import { configureStore } from '@reduxjs/toolkit';
+import missionsReducer from './missions/MissionSlice';
 
 const store = configureStore({
-  reducer: rootReducer,
-});
-
-const setupStore = (preloadedState) => configureStore({
-  reducer: rootReducer,
-  preloadedState,
+  reducer: {
+    missions: missionsReducer,
+  },
 });
 
 export default store;
-export { setupStore };
