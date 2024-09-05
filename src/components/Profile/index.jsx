83896ae4bcebfb342c3502bd6bd0f4d7a05e7545
Shegofa <shogofadeveloper12@gmail.com>
2024-09-05 07:40:44 +0400

    Add ProfileList component for displaying user reservations

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
-  return <p>My MyProfilem</p>;
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
+  return (
+    <div>
+      <h1>My Rockets</h1>
+      <ul>
+        {reservedRockets.length > 0 ? (
+          reservedRockets.map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))
+        ) : (
+          <li>No reserved rockets</li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default MyProfile;
