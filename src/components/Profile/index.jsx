68bdee08f3b23a63dce6177edfdda2f1f6940ca8
unknown <royajamal2003@gmail.com>
2024-09-05 05:57:02 -0700

    index.jsx file design with tailwind

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function MyProfile() {
-  return <p>My MyProfilem</p>;
-}
+const MyProfile = () => {
+  const selectReservedRockets = (state) => state.rockets.rockets.filter((rocket) => rocket.reserved);
+  const selectReservedMissions = (state) => state.missions.reservedMissions.map((missionId) => state.missions.missions.find((mission) => mission.id === missionId));
+
+  const reservedRockets = useSelector(selectReservedRockets);
+  const reservedMissions = useSelector(selectReservedMissions);
+
+  return (
+    <div className="profiles">
+      <div className="rockets">
+        <h2>My Rockets</h2>
+        <table className="rocket-table">
+          <tbody>
+            {reservedRockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td>{rocket.rocket_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="missions">
+        <h2>My Missions</h2>
+        <table className="rocket-table">
+          <tbody>
+            {reservedMissions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
