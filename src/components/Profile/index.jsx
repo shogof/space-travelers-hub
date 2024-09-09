f7617d7543e59b3550f5fc6361a8d375c32f4a33
Shegofa <shogofadeveloper12@gmail.com>
2024-09-09 15:39:58 +0400

    profile style changed

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -10,8 +10,8 @@ function MyProfile() {
 
   return (
     <div className="border-purple-500 border-2 w-2/4 mx-auto p-8 h-auto">
-      <h1 className="text-4xl mb-8 text-center">My Profile</h1>
-      <div className="mb-8">
+      <h1 className="text-5xl mb-8 text-center">My Profile</h1>
+      <div className="mb-8 border-purple-600 border-2 py-4 outline-double">
         <h2 className="text-3xl mb-4 text-center">My Rockets</h2>
         <ul>
           {reservedRockets.length > 0 ? (
@@ -24,13 +24,13 @@ function MyProfile() {
               </li>
             ))
           ) : (
-            <li className="text-3xl text-center font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">
+            <li className="text-2xl text-center font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent py-5">
               No reserved rockets
             </li>
           )}
         </ul>
       </div>
-      <div>
+      <div className="border-purple-900 outline-double border-2 py-4">
         <h2 className="text-3xl mb-4 text-center">My Missions</h2>
         <ul>
           {reservedMissionsList.length > 0 ? (
@@ -43,7 +43,7 @@ function MyProfile() {
               </li>
             ))
           ) : (
-            <li className="text-3xl text-center font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">
+            <li className="text-2xl text-center font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent  py-5">
               No reserved missions
             </li>
           )}
