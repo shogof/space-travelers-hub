c4651eb560dbd2c4a2081989944000a9ed35e4ee
Shegofa <shogofadeveloper12@gmail.com>
2024-09-09 15:08:12 +0400

    Resolved issues related to merging branches, including errors with rocket and mission data handling.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import RocketsSlice from './Rockets/RocketsSlice';
 import missionsReducer from './missions/MissionSlice';
 
+// Combine reducers if you have multiple slices
+const rootReducer = combineReducers({
+  rockets: RocketsSlice,
+  missions: missionsReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    missions: missionsReducer,
-  },
+  reducer: rootReducer, // Use rootReducer to combine reducers
+});
+
+const setupStore = (preloadedState) => configureStore({
+  reducer: rootReducer, // Ensure consistency with rootReducer
+  preloadedState,
 });
 
 export default store;
+export { setupStore };
