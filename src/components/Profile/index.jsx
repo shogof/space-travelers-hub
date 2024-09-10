ddab99531818bef4e61984e78fae64717cf53310
unknown <marwasadat735@gmail.com>
2024-09-10 20:58:01 +0330

    p/index.jsx/c..,

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  // Get dragons from the state instead of rockets
   const { dragons } = useSelector((state) => state.dragons);
   const reservedDragons = dragons.filter((dragon) => dragon.reserved);
 
   return (
-    <div className="border-purple-500 border-2 w-2/4 mx-auto p-8 h-auto">
+    <div className="border-purple-500 border-2 w-2/4 mx-auto p-8 h-auto ">
       <h1 className="text-4xl mb-8 text-center">My Dragons</h1>
       <ul>
         {reservedDragons.length > 0 ? (
           reservedDragons.map((dragon) => (
             <li
               key={dragon.id}
-              className="border-fuchsia-500 border-2 w-4/5 mx-auto p-3 my-6"
+              className=" border-fuchsia-500 border-2 w-4/5 mx-auto p-3 my-6"
             >
               {dragon.name}
             </li>
